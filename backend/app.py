caa7d8080bb4623dec793c80dc0aedd99d407d41
indexjoseph <joladeji03@gmail.com>
2023-07-24 23:57:19 -0400

    Setting up database connection between flask local server and MongoDB

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,32 @@
+# from dotenv import load_dotenv
+from pymongo import MongoClient
+from flask_pymongo import PyMongo
 from flask import Flask
+# import os
+# from pathlib import Path
+# app = Flask(__name__)
 
-app = Flask(__name__)
+# mongo = PyMongo(app)
+uri = "URI"
+# app.config['MONGO_URI'] = uri
 
+# Set the Stable API version when creating a new client
+client = MongoClient(uri,
+                     tls=True,
+                     tlsCertificateKeyFile="CERTIFICATE")
+                          
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
-@app.route('/')
-def main():
-    return '<h1>WCU CS Club</h1>'
+# Replace the placeholder with your Atlas connection string
+
+# Send a ping to confirm a successful connection
+
+# @app.route('/')
+# def hello():
+#     return '<h1>WCU CS Club</h1>'
+
