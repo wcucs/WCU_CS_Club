76b4d328a54c4dd6c5d880728d57d81d644eac60
Nolan Flinchum <nolan@flinchum.net>
2023-07-30 17:12:00 -0400

    continued homepage development using jsx

--- frontend/wcu-cs-club-site/src/App.js
+++ frontend/wcu-cs-club-site/src/App.js
@@ -1,14 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src="https://lh3.googleusercontent.com/drive-viewer/AITFw-zYWVsapxYiflnOcugJPE3HelU67We-65Krn1LI71JuQwkeFI6nQZsp8S8ct_s-zMbAl-uJ0kcjmnf83ZutFE59otiLjQ=w1138-h962" className="App-logo" alt="logo" />
+
+      <div className="Menu-bar">
+        <div>
+          <img src ="https://cdn.discordapp.com/attachments/1020055404148961355/1135305104837320745/AITFw-wtZ0VsYHLjq704E1uwf3AIwklLHcHKudnZjjCMkFdRbH8BLo7ri3BnxvfrHtrPmDUtKCYY737nevTRCv0qpsyK6TApw2560-h962.png" className="Corner-logo" alt="logo" /> 
+        </div>
+        <div>
+          <button className="Standard-button">About Us</button>
+          <button className="Standard-button">Events</button>
+          <button className="Standard-button">Resources</button>
+          <button className="Purple-button">Join</button>
+        </div>
+      </div>
+
+      <img src="https://cdn.discordapp.com/attachments/1020055404148961355/1135308674395557958/wccsc-logo3.png" className="Home-graphic" alt="homepage" />
+
+      <div className="Events-bar">
+        <img src="https://cdn.discordapp.com/attachments/1020055404148961355/1135314055347834990/group-photo-event.png" className="" alt="group-event" />
+        <div className="App">
+          <h1 className="Event-header">Upcoming Event: TBD</h1>
+          <button className="Gold-button">Find out more</button>
+        </div>
+      </div>
+      
+      <header className="App-header">  
         <p>
-          Welcome to the WCU CS Club Website!!!!!!
+          About Us
         </p>
         <a
           className="App-link"
@@ -18,6 +38,7 @@ function App() {
         >
           free robux
         </a>
+        <img src="https://lh3.googleusercontent.com/drive-viewer/AITFw-zYWVsapxYiflnOcugJPE3HelU67We-65Krn1LI71JuQwkeFI6nQZsp8S8ct_s-zMbAl-uJ0kcjmnf83ZutFE59otiLjQ=w1138-h962" className="App-logo" alt="logo" />  
       </header>
     </div>
   );
